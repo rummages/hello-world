5e76142cf101619d8f16692f7366470740e69f89
rummages <e4b816@gmail.com>
2015-12-04 10:39:44 +0000

    Added some comments

--- hello.go
+++ hello.go
@@ -1,8 +1,20 @@
 package main
 
+// Package declaration, every program must start with one and they identify and help reuse code 
+
 import "fmt"
 
+// Import is a keyword that tells go to import code from other packages
+
+// This is obviously comments for single lines 
+
+/* and this 
+is multiline
+comments */ 
+
 func main() {
+// functions are the building block of go and main is special as it gets called when executed
     fmt.Printf("hello, world\n")
+// https://golang.org/pkg/fmt/ Package fmt implements formatted I/O with functions analogous to C's printf and scanf
 }
 
