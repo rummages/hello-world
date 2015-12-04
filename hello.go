cda2b54d96146ad83837f2975a0c78370dc04fdc
rummages <e4b816@gmail.com>
2015-12-04 11:06:13 +0000

    Comments and notes

--- hello.go
+++ hello.go
@@ -4,7 +4,7 @@ package main
 
 import "fmt"
 
-// Import is a keyword that tells go to import code from other packages
+// Import is a keyword that tells go to import code from other packages or libraries
 
 // This is obviously comments for single lines 
 
@@ -18,3 +18,15 @@ func main() {
 // https://golang.org/pkg/fmt/ Package fmt implements formatted I/O with functions analogous to C's printf and scanf
 }
 
+/* Created an executable program that imports the 'fmt' package ( docs can be found at https://golang.org/pkg/fmt/ ) 
+Several practice comment lines to help me learn, and to get into the habit of documenting codebase
+Function Main does one thing, print out the string hello world as is standard starting programming practice
+You can also learn more by using godoc at the command line i.e. godoc fmt Println and it will provide documentation 
+$ godoc fmt Println
+func Println(a ...interface{}) (n int, err error)
+    Println formats using the default formats for its operands and writes to
+    standard output. Spaces are always added between operands and a newline
+    is appended. It returns the number of bytes written and any write error
+    encountered.
+Also did a go run to compile and run the executable, everything went fine. 
+*/ 
